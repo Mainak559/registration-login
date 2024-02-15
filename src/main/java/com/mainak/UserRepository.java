ecf89458a1cd4651cac72b2be6f961f0861f2a5f
Mainak Halder <mainakhalder5598633@gmail.com>
2024-02-15 14:01:34 +0530

    registration-login 1.0

---
+++ src/main/java/com/mainak/UserRepository.java
@@ -0,0 +1,9 @@
+package com.mainak;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	public User findByEmail(String email);	
+}
