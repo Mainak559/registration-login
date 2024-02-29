b66c46f08bf808fef648bd224009d0899d674ee0
Mainak Halder <mainakhalder5598633@gmail.com>
2024-02-29 12:18:56 +0530

    push with azure changes

--- src/main/java/com/mainak/User.java
+++ src/main/java/com/mainak/User.java
@@ -1,11 +1,12 @@
 package com.mainak;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "users")
